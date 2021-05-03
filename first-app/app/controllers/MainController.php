e13b7c5d3fe6fe9832bbcbcbdb58acc2d0e04533
Louis-Amand <21807526@etu.unicaen.fr>
2021-05-03 14:35:01 +0200

    creation MyAuth + MainController + redefinir messages

---
+++ first-app/app/controllers/MainController.php
@@ -0,0 +1,14 @@
+<?php
+namespace controllers;
+
+ /**
+ * Controller MainController
+ **/
+class MainController extends ControllerBase{
+
+    #[Route('_default', name: 'home')]
+	public function index(){
+        
+	}
+
+}
